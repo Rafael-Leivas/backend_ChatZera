8323ee981141a9e440639166c648c041fc0faddd
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2024-08-30 16:30:52 -0300

    Conection with method send_message

--- backend_chatzera/api/api.py
+++ backend_chatzera/api/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from backend_chatzera.server.produtor_mensagens import ProdutorMensagens
 
 app = FastAPI()
 
@@ -16,6 +18,14 @@ app.add_middleware(
 def read_root():
     return {'message': 'Hello World'}
 
+class ChatMessage(BaseModel):
+    message: str
+    sender_name: str
+
 @app.post('/chat')
-def send_message():
-    return {'message': 'Message sent'}
+def send_message(chat_message: ChatMessage):
+
+    produtor = ProdutorMensagens()
+    produtor.send_message(chat_message.message, chat_message.sender_name)
+
+    return {'Mensagem enviada': chat_message.message}
