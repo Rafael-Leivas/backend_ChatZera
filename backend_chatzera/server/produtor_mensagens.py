899f7b08335871272d3462f5e3494c57a5c2a412
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2024-08-30 14:12:33 -0300

    constructing productor message

---
+++ backend_chatzera/server/produtor_mensagens.py
@@ -0,0 +1,28 @@
+import pika
+
+class ProdutorMensagens:
+    QUEUE_NAME = "msg-in"
+    EXCHANGE_GROUP = "clientes1-3"
+
+    def __init__(self):
+        self.factory = pika.ConnectionParameters(
+            host="189.8.205.54",
+            virtual_host="thanos",
+            credentials=pika.PlainCredentials("senai", "senai@")
+        )
+
+    def send_message(self, msg, sender_name):
+        with pika.BlockingConnection(self.factory) as connection:
+            channel = connection.channel()
+
+            channel.exchange_declare(exchange=self.EXCHANGE_GROUP, exchange_type='fanout', durable=True)
+
+            property = pika.BasicProperties(headers={'sender_name': sender_name})
+
+            channel.basic_publish(exchange=self.EXCHANGE_GROUP, routing_key='', body=msg.encode())
+            print(f" [x] Enviado...: '{msg}' de '{sender_name}'")
+
+
+if __name__ == "__main__":
+    produtor = ProdutorMensagens()
+    produtor.send_message("Hello World", "Backend")
